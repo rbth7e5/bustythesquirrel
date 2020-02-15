72fea6315276b7b4338d1ae53571ed66794a60b4
rbth7e5 <caijie96@gmail.com>
2020-02-15 11:26:50 -0500

    Welcome page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,45 @@
 import React from 'react';
 import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
+  BrowserRouter as Router,
+  Switch,
+  Route,
 } from 'react-router-dom';
 import './App.css';
 import Welcome from './Welcome';
 
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      // light: will be calculated from palette.primary.main,
+      main: '#2e3546',
+      light: '#283d70',
+      // dark: will be calculated from palette.primary.main,
+      // contrastText: will be calculated to contrast with palette.primary.main
+    },
+    secondary: {
+      main: '#703529',
+      light: '#c6934b',
+    },
+    background: {
+      default: '#e0ddd5',
+    }
+  },
+});
+
 function App() {
   return (
+    <ThemeProvider theme={theme}>
     <Router>
-        <Switch>
-            <Route path="/">
-                <Welcome/>
-            </Route>
-        </Switch>
+      <Switch>
+        <Route path="/">
+          <Welcome/>
+        </Route>
+      </Switch>
     </Router>
+    </ThemeProvider>
   );
 }
 
--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -1,16 +1,51 @@
-import {Paper, TextField} from "@material-ui/core";
+import {Button, Card, CardActions, CardContent, TextField, Typography} from "@material-ui/core";
 import React from "react";
+import {makeStyles} from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    height: '100vh',
+    width: '100vw',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: theme.palette.background.default,
+  },
+  card: {
+    padding: 32,
+    backgroundColor: theme.palette.background.default,
+  },
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  textfield: {
+    marginBottom: 48,
+  },
+  button: {
+    margin: 'auto'
+  }
+}));
 
 export default function Welcome() {
-    return (
-        <div className="Welcome">
-            <Paper className="Login">
-                <h1 style={{flex: 2, padding: 32}}>Welcome</h1>
-                <div className="Form">
-                    <TextField label="Username"/>
-                    <TextField label="Password"/>
-                </div>
-            </Paper>
-        </div>
-    )
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Card className={classes.card}>
+        <CardContent style={{padding: 8}}>
+          <Typography className={classes.textfield} variant="h3" component="h1">
+            Welcome to Agora
+          </Typography>
+          <form className={classes.form}>
+            <TextField className={classes.textfield} label="Username" variant="outlined"/>
+            <TextField className={classes.textfield} label="Password" variant="outlined"/>
+          </form>
+        </CardContent>
+        <CardActions disableSpacing>
+          <Button fullWidth color="secondary" className={classes.button} variant="contained">Log me in!</Button>
+        </CardActions>
+      </Card>
+    </div>
+  )
 }
